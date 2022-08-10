991fa98898af98f5ee2f5a8fc97475faf178e127
lhuzarski1 <lukaszhuzarski1@gmail.com>
2022-08-10 10:04:45 +0200

    SSR wip

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import { render } from "react-dom";
-// import SearchParams from "./SearchParams";
-import { StrictMode, useState, lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-// import Details from "./Details";
+import SearchParams from "./SearchParams";
+import { StrictMode, useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
 import ThemeContext from "./ThemeContext";
-
-const Details = lazy(() => import('./Details'))
-const SearchParams = lazy(() => import('./SearchParams'))
 
 const App = () => {
   const theme = useState("darkblue");
   return (
     <StrictMode>
-      <Suspense fallback={<h2>loading...</h2>}>
       <ThemeContext.Provider value={theme}>
-        <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
       </ThemeContext.Provider>
-      </Suspense>
     </StrictMode>
   );
 };
 
-render(<App />, document.getElementById("root"));
+export default App;
